82735cf7608c30f88a5480bcc67f906173caa486
Enrique Gómez Bazúa <enriquegbazua@gmail.com>
2021-09-03 18:29:32 -0700

    Final details

--- src/components/Firestore.jsx
+++ src/components/Firestore.jsx
@@ -1,163 +1,154 @@
-import React from 'react'
-import { db } from '../firebase'
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
 
-const Firestore = (props) => {
+function Firestore(props) {
 
-    const [tareas, setTareas] = React.useState([])
-    const [tarea, setTarea] = React.useState('')
-    const [modoEdicion, setModoEdicion] = React.useState(false)
-    const [id, setId] = React.useState('')
-  
-  
-    React.useEffect(() => {
-  
-      const obtenerDatos = async () => {
-  
-        try {
-  
-          const data = await db.collection(props.user.uid).get()
-          const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-          console.log(arrayData)
-          setTareas(arrayData)
-          
-        } catch (error) {
-          console.log(error)
-        }
-  
-      }
-  
-      obtenerDatos()
-  
-    }, [])
-  
-    const agregar = async (e) => {
-      e.preventDefault()
-  
-      if(!tarea.trim()){
-        console.log('está vacio')
-        return
-      }
-  
-      try {
-  
-        const nuevaTarea = {
-          name: tarea,
-          fecha: Date.now()
-        }
-        const data = await db.collection(props.user.uid).add(nuevaTarea)
-  
-        setTareas([
-          ...tareas,
-          {...nuevaTarea, id: data.id}
-        ])
-  
-        setTarea('')
-        
-      } catch (error) {
-        console.log(error)
-      }
-  
-      console.log(tarea)
-    }
-  
-    const eliminar = async (id) => {
+  const [task, setTask] = useState([]);
+  const [taskForm, setTaskForm] = useState('');
+  const [editMode, setEditMode] = useState(false);
+  const [id, setId] = useState('');
+ 
+  useEffect(() => {
+    
+    const getData = async () => {
+
       try {
         
-        await db.collection(props.user.uid).doc(id).delete()
-  
-        const arrayFiltrado = tareas.filter(item => item.id !== id)
-        setTareas(arrayFiltrado)
-  
+        const data = await db.collection(props.user.uid).get()
+        const dataArray = data.docs.map( (doc) => ({id: doc.id, ...doc.data() }));
+        /* console.log(dataArray); */
+        setTask(dataArray);
+
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-  
-    const activarEdicion = (item) => {
-      setModoEdicion(true)
-      setTarea(item.name)
-      setId(item.id)
-    }
-  
-    const editar = async (e) => {
-      e.preventDefault()
-      if(!tarea.trim()){
-        console.log('vacio')
-        return
-      }
-      try {
-        
-        await db.collection(props.user.uid).doc(id).update({
-          name: tarea
-        })
-        const arrayEditado = tareas.map(item => (
-          item.id === id ? {id: item.id, fecha: item.fecha, name: tarea} : item
-        ))
-        setTareas(arrayEditado)
-        setModoEdicion(false)
-        setTarea('')
-        setId('')
-      } catch (error) {
-        console.log(error)
-      }
+
     }
 
-    return (
-        <div>
-            <div className="row">
-                <div className="col-md-6">
-                    <h3 className="mt-2">Lista de tareas</h3>
-                    <ul className="list-group">
-                        {
-                        tareas.map(item => (
-                            <li className="list-group-item" key={item.id}>
-                            {item.name}
-                            <button 
-                                className="btn btn-danger btn-sm float-end me-2"
-                                onClick={() => eliminar(item.id)}
-                            >
-                                Eliminar
-                            </button>
-                            <button 
-                                className="btn btn-warning btn-sm float-end me-2"
-                                onClick={() => activarEdicion(item)}
-                            >
-                                Editar
-                            </button>
-                            </li>
-                        ))
-                        }
-                    </ul>
-                </div>
-                <div className="col-md-6">
-                    <h3 className="mt-2">
-                        {
-                        modoEdicion ? 'Editar Tarea' : 'Agregar Tarea'
-                        }
-                    </h3>
-                    <form onSubmit={modoEdicion ? editar : agregar}>
-                        <input 
-                        type="text"
-                        placeholder="Ingrese tarea"
-                        className="form-control mb-2"
-                        onChange={e => setTarea(e.target.value)}
-                        value={tarea}
-                        />
-                        <button 
-                        className={
-                            modoEdicion ? 'btn btn-warning btn-block' : 'btn btn-dark btn-block'
-                        }
-                        type="submit"
-                        >
-                        {
-                            modoEdicion ? 'Editar' : 'Agregar'
-                        }
-                        </button>
-                    </form>
-                </div>
-            </div> 
+    getData();
+
+  }, []);
+
+  const addTask = async (e) => {
+    e.preventDefault();
+    /* console.log(task); */
+
+    if(!taskForm.trim()){
+      return console.log('Empty');
+    }
+
+    try {
+      
+      const newTask = {
+        name: taskForm,
+        date: Date.now()
+      }
+
+      const data = await db.collection(props.user.uid).add(newTask);
+
+      setTask([
+        ...task,
+        {...newTask, id: data.id}
+      ])
+
+      setTaskForm('');
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log(taskForm);
+  }
+
+  const deleteTask = async (idTask) => {
+    try {
+      
+      await db.collection(props.user.uid).doc(idTask).delete();
+
+      const filterArray = task.filter(item => item.id !== idTask);
+      setTask(filterArray);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const activateEditMode = (item) => {
+    setEditMode(true);
+    setTaskForm(item.name);
+    setId(item.id);
+  }
+
+  const editTask = async (e) => {
+    e.preventDefault();
+    
+    if(!taskForm.trim()){
+      console.log('empty')
+      return
+    }
+
+    try {
+      
+      await db.collection(props.user.uid).doc(id).update({
+        name: taskForm
+      });
+
+      const editedArray = task.map(item => (
+        item.id === id ? {id: item.id, date: item.date, name: taskForm} : item
+      ))
+
+      setTask(editedArray);
+      
+      setEditMode(false);
+      setTaskForm('');
+      setId('');
+
+    } catch (error) {
+      console.log(error);  
+    }
+
+  }
+
+  return (
+    <div className="container">
+      <h1 className="text-center mt-1"> • ADMINSTRATION • </h1>
+      <hr className="mb-4"/>
+      <div className="row">
+        <div className="col-md-6">
+          <ul className="list-group">
+            {
+              task.map(item => (
+                <li className="list-group-item" key={item.id}>
+                  {item.name}
+                  <button onClick={ () => deleteTask(item.id)} className="btn btn-danger btn-sm float-end ms-2">
+                    Delete
+                  </button>
+                  <button onClick={ () => activateEditMode(item)} className="btn btn-warning btn-sm float-end">
+                    Edit
+                  </button>
+                </li>
+              ))
+            }
+          </ul>
         </div>
-    )
+        <div className="col-md-6">
+          <h3 className="text-center">
+            {
+              editMode ? 'Edit Task' : 'Add Task'
+            }
+          </h3>
+          <form onSubmit={ editMode ? editTask : addTask }>
+            <input onChange={e => setTaskForm(e.target.value)} value={taskForm} type="text" placeholder="Enter Task" className="form-control mb-2" />
+            <button className={ editMode ? 'btn btn-warning w-100' : 'btn btn-dark w-100' } type="submit" >
+              {
+                editMode ? 'Edit' : 'Add'
+              }
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Firestore
+export default Firestore;
